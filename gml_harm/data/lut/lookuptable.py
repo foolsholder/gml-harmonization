aab0a767448449b8b0d38ba82538a501aff3cd20
theherowho <myarctan@gmail.com>
2021-12-16 10:39:24 +0300

    lut dataset

---
+++ gml_harm/data/lut/lookuptable.py
@@ -0,0 +1,33 @@
+import torch
+import trilinear_cpp
+
+import numpy as np
+
+from torch import nn
+
+
+class LookUpTable3D(nn.Module):
+    def __init__(self, dim=33):
+        super(LookUpTable3D, self).__init__()
+        if dim == 33:
+            file = open("IdentityLUT33.txt", 'r')
+        elif dim == 64:
+            file = open("IdentityLUT64.txt", 'r')
+        lines = file.readlines()
+        buffer = np.zeros((3, dim, dim, dim), dtype=np.float32)
+
+        for i in range(0, dim):
+            for j in range(0, dim):
+                for k in range(0, dim):
+                    n = i * dim * dim + j * dim + k
+                    x = lines[n].split()
+                    buffer[0, i, j, k] = float(x[0])
+                    buffer[1, i, j, k] = float(x[1])
+                    buffer[2, i, j, k] = float(x[2])
+        self.LUT = nn.Parameter(torch.from_numpy(buffer).requires_grad_(True))
+        self.TrilinearInterpolation = TrilinearInterpolation()
+
+    def forward(self, x):
+        _, output = self.TrilinearInterpolation(self.LUT, x)
+        # self.LUT, output = self.TrilinearInterpolation(self.LUT, x)
+        return output
---
+++ gml_harm/data/lut_dataset.py
@@ -0,0 +1,8 @@
+import torch
+
+
+from torch.utils.data import Dataset
+
+from .base_dataset import ABCDataset
+
+
